e9a4464c3b992fb4b01deb7da213ea6c44e6ddb8
Vsevolod <seva151201@gmail.com>
2022-04-12 17:37:54 +0700

    Add password encryption

--- src/main/java/com/schedule/user/service/CreateUserServiceImpl.java
+++ src/main/java/com/schedule/user/service/CreateUserServiceImpl.java
@@ -4,6 +4,7 @@ import com.schedule.user.model.entity.User;
 import com.schedule.user.model.request.CreateUserRequest;
 import com.schedule.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -12,13 +13,14 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor
 public class CreateUserServiceImpl implements CreateUserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User create(CreateUserRequest createUserRequest) {
         return userRepository.save(
                 new User(
                         createUserRequest.getLogin(),
-                        createUserRequest.getPassword(),
+                        passwordEncoder.encode(createUserRequest.getPassword()),
                         LocalDateTime.now(),
                         createUserRequest.getEmail()
                 )
